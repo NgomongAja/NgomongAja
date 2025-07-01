71befec8fd929fd752690b9596e12115c392f43a
9William <100459420+9William@users.noreply.github.com>
2025-07-01 17:20:11 +0700

    feat(trending): Add Bikin Threads for current user

--- lib/common/ui/general_bottomsheet.dart
+++ lib/common/ui/general_bottomsheet.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class GeneralBottomsheet {
+  // Membuat class yang nantinya akan dipanggil untuk digunakan sebagai form contoh New Post untuk Thread
+  // Contoh Pemakaian pada trending_presenter method addNewThread yang memanggil widget child = ThreadInput
+  // yang menggunakan inputan dari widget thread_input
+
   static void showGeneralbottomsheet(BuildContext context, Widget child) {
     showModalBottomSheet(
       context: context,
